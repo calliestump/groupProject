6e3caf89f5b4968ab8ab0861dda19a6ca53716a6
Callie Stump <callie@stu.mp>
2020-10-22 17:09:59 -0400

    adds elements to indexhtml and subpages using js,css
    Co-authored-by: Ryland Adams <rylandadams@yahoo.com>
    Co-authored-by: Ellie Lambert <ebezjian@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -21,6 +21,7 @@ function divide(number1, number2) {
 //Character
 
 
+
 //Celeb Dating
 
 
@@ -54,12 +55,74 @@ $(document).ready(function() {
     }
     $("#output").text(result);
   });
-});
 
 //Character
+  $("form#age").submit(function(event) {
+    event.preventDefault();
+    const personality = $("select#personality").val();
+    const favecolor = $("select#favecolor").val();
+
+    if (personality === "happy" && favecolor === "red") {
+      $('#under-18').hide();
+      $('#legal').show();
+    } else if (personality === "happy" && favecolor === "blue") {
+      $('#legal').hide();
+      $('#under-18').show();
+    } else if (personality === "happy" && favecolor === "black") {
+      $('#legal').hide();
+      $('#under-18').show();
+    } else if (personality === "happy" && favecolor === "yellow") {
+      $('#legal').hide();
+      $('#under-18').show();
+    } else if (personality === "sad" && favecolor === "red") {
+      $('#legal').hide();
+      $('#under-18').show();
+    } else if (personality === "sad" && favecolor === "blue") {
+      $('#legal').hide();
+      $('#under-18').show();
+    } 
+    else if (personality === "sad" && favecolor === "black") {
+      $('#legal').hide();
+      $('#under-18').show();
+    }   else if (personality === "sad" && favecolor === "blue") {
+      $('#legal').hide();
+      $('#under-18').show();
+    }          
+  });
+
 
 
 //Celeb Dating
+  $("form#matchmaking").submit(function(event) {
+    event.preventDefault();
+    const age = parseInt($("input#age").val());
+    const hair = $("select#hair").val();
+
+    let image ="";
+      if (age === 'blonde' $$ age <= 100); {
+        $('kodku1').show();
+      }
+      if (age === 'red' $$ age <= 100); {
+        $('kodku2').show();
+      }
+      if (age === 'dark' $$ age <= 100); {
+        $('kodku5').show();
+      }
+      if (age === 'other' $$ age <= 100); {
+        $('kodku4').show();
+      } 
+      
+      $("#picture").show();
+
+  });
+  
 
 
 //Triangle Tracker
+  $("form#age").submit(function(event) {
+    event.preventDefault();
+    const side1 = parseInt($("input#side1").val());
+    const side2 = parseInt$("input#side2").val();
+    const side1 = parseInt($("input#side3").val());
+
+});
