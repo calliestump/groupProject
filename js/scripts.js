ea69214906a8f931e6498ccb24ddbe352c9b3136
Callie Stump <callie@stu.mp>
2020-10-22 19:03:03 -0400

    updates subpages with elements
    Co-authored-by: Ryland Adams <rylandadams@yahoo.com>
    Co-authored-by: Ellie Lambert <ebezjian@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -39,7 +39,6 @@ function divide(number1, number2) {
 //Calculator
 $(document).ready(function() {
   $("form#calculator").submit(function() {
-    event.preventDefault();
     const number1 = parseInt($("#input1").val());
     const number2 = parseInt($("#input2").val());
     const operator = $("input:radio[name=operator]:checked").val();
@@ -57,72 +56,101 @@ $(document).ready(function() {
   });
 
 //Character
-  $("form#age").submit(function(event) {
-    event.preventDefault();
-    const personality = $("select#personality").val();
-    const favecolor = $("select#favecolor").val();
+  $("form#cartoon").submit(function(event) {
+    var personality = parseInt($("select#personality").val());
+    var favecolor = parseInt($("select#favecolor").val());
 
-    if (personality === "happy" && favecolor === "red") {
-      $('#under-18').hide();
-      $('#legal').show();
-    } else if (personality === "happy" && favecolor === "blue") {
-      $('#legal').hide();
-      $('#under-18').show();
-    } else if (personality === "happy" && favecolor === "black") {
-      $('#legal').hide();
-      $('#under-18').show();
-    } else if (personality === "happy" && favecolor === "yellow") {
-      $('#legal').hide();
-      $('#under-18').show();
-    } else if (personality === "sad" && favecolor === "red") {
-      $('#legal').hide();
-      $('#under-18').show();
-    } else if (personality === "sad" && favecolor === "blue") {
-      $('#legal').hide();
-      $('#under-18').show();
-    } 
-    else if (personality === "sad" && favecolor === "black") {
-      $('#legal').hide();
-      $('#under-18').show();
-    }   else if (personality === "sad" && favecolor === "blue") {
-      $('#legal').hide();
-      $('#under-18').show();
-    }          
+    if (personality === 1 && favecolor === 2) {
+      $().hide();
+      $('#pic1').show();
+    } else if (personality === 1 && favecolor === 1) {
+      $().hide();
+      $('#pic2').show();
+    } else if (personality === 1 && favecolor === 3) {
+      $().hide();
+      $('#pic3').show();
+    } else if (personality === 1 && favecolor === 4) {
+      $().hide();
+      $('#pic4').show();
+    } else if (personality === 2 && favecolor === 2) {
+      $().hide();
+      $('#pic5').show();
+    } else if (personality === 2 && favecolor === 1) {
+      $().hide();
+      $('#pic6').show();
+    } else if (personality === 2 && favecolor === 3) {
+      $().hide();
+      $('#pic7').show();
+    } else if (personality === 2 && favecolor === 4) {
+      $().hide();
+      $('#pic8').show();
+    } else if (personality === 3 && favecolor === 2) {
+      $().hide();
+      $('#pic9').show();
+    } else if (personality === 3 && favecolor === 1) {
+      $().hide();
+      $('#pic10').show();
+    } else if (personality === 3 && favecolor === 3) {
+      $().hide();
+      $('#pic11').show();
+    } else if (personality === 3 && favecolor === 4) {
+      $().hide();
+      $('#pic12').show();
+    } else if (personality === 4 && favecolor === 2) {
+      $().hide();
+      $('#pic13').show();
+    } else if (personality === 4 && favecolor === 1) {
+      $().hide();
+      $('#pic14').show();
+    } else if (personality === 4 && favecolor === 3) {
+      $().hide();
+      $('#pic15').show();
+    } else if (personality === 4 && favecolor === 4) {
+      $().hide();
+      $('#pic16').show();
+    }                           
   });
+  event.preventDefault(); 
+});
+ 
+// //Celeb Dating
+//   $("form#matchmaking").submit(function(event) {
 
+//     const age = parseInt($("input#age").val());
+//     const hair = parseInt($("select#hair").val());
 
-
-//Celeb Dating
-  $("form#matchmaking").submit(function(event) {
-    event.preventDefault();
-    const age = parseInt($("input#age").val());
-    const hair = $("select#hair").val();
-
-    let image ="";
-      if (age === 'blonde' $$ age <= 100); {
-        $('kodku1').show();
-      }
-      if (age === 'red' $$ age <= 100); {
-        $('kodku2').show();
-      }
-      if (age === 'dark' $$ age <= 100); {
-        $('kodku5').show();
-      }
-      if (age === 'other' $$ age <= 100); {
-        $('kodku4').show();
-      } 
-      
-      $("#picture").show();
-
-  });
+    
+//     if (hair === "1" && age < 100) {
+//       $('#pic2').show();
+    
+//     } else if (hair === "2" && age < 100) {
+//       $('pic3').show();
+    
+//     } else if (hair === "3" && age < 100) {
+//       $('pic7').show();
+    
+//     } else if (hair === "4" && age < 100) {
+//       $('pic5').show();
+//     $("#submit").click(function() {
+//     $("#picture").show();
+//     event.preventDefault();
+//     }
+//   };
   
 
 
-//Triangle Tracker
-  $("form#age").submit(function(event) {
-    event.preventDefault();
-    const side1 = parseInt($("input#side1").val());
-    const side2 = parseInt$("input#side2").val();
-    const side1 = parseInt($("input#side3").val());
-
-});
+// //Triangle Tracker
+//   $("form#equal").submit(function(event) {
+//     event.preventDefault();
+//     const side1 = parseInt($("#input1").val());
+//     const side2 = parseInt($("#input2").val());
+//     const side3 = parseInt($("#input3").val());
+//     let triResult;
+//     if (side1 == side2); {
+//       triResult = "equilateral"
+//     }
+    
+//     $("#outputequal").text(triResult);
+//   }
+// });
+// }); 
